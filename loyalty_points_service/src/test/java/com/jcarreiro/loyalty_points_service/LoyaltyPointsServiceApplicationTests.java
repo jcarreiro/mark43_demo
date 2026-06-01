46e5d3787fe8761e69eac831f2b36b68444115c7
Jason Carreiro <jcarreiro@gmail.com>
2026-06-01 09:55:37 -0400

    [loyalty_points_service] Initial commit

---
+++ loyalty_points_service/src/test/java/com/jcarreiro/loyalty_points_service/LoyaltyPointsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcarreiro.loyalty_points_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyPointsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
