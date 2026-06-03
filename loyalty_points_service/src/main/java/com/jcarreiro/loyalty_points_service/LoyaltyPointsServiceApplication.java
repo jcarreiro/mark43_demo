add590375a42828bb6e7a7bfe5089775da50ca90
Jason Carreiro <jcarreiro@gmail.com>
2026-06-03 14:28:07 -0400

    [points] Remove hello endpoint

--- loyalty_points_service/src/main/java/com/jcarreiro/loyalty_points_service/LoyaltyPointsServiceApplication.java
+++ loyalty_points_service/src/main/java/com/jcarreiro/loyalty_points_service/LoyaltyPointsServiceApplication.java
@@ -2,8 +2,6 @@ package com.jcarreiro.loyalty_points_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,10 +10,5 @@ public class LoyaltyPointsServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LoyaltyPointsServiceApplication.class, args);
-  }
-
-  @GetMapping("/hello")
-  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return String.format("Hello %s!", name);
   }
 }
